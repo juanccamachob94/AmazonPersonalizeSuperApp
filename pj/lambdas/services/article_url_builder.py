2194407fe004b551905233d3bc055f15bb4f006f
Juan Camilo <juanccamachob94@gmail.com>
2022-04-29 16:19:14 -0500

    fix slug category

--- pj/lambdas/services/article_url_builder.py
+++ pj/lambdas/services/article_url_builder.py
@@ -1,11 +1,12 @@
 from slugify import slugify
 # considering ADN40 in all news (articles)
+
 class ArticleUrlBuilder:
-    DOMAIN = "https://s3.amazonaws.com/4pp.adn40.com/app/"
+    DOMAIN = "https://s3.amazonaws.com/4pp.adn40.com/app/2020/detalles/"
 
     @classmethod
     def perform(cls, article):
         slug_title = slugify(article.get_title())
         # year = item_dict['publication_date'][0:4]
-        category = article.get_category().lower()
-        return f'{cls.DOMAIN}2020/detalles/{category}/{slug_title}.json'
+        slug_category = slugify(article.get_category().lower())
+        return f'{cls.DOMAIN}{slug_category}/{slug_title}.json'
